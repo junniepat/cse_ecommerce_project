9f9f218a97a247853daa8e4c90a2572eca37f058
junniepat <patrickoigwe@gmail.com>
2022-01-29 21:41:18 +0400

    make it better

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-
+const cors = require('cors')
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const corsOptions = {
+  origin: "https://cse341-patrick.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
 
 const errorController = require('./controllers/error');
 
@@ -23,4 +30,26 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+
+const options = {
+  family: 4
+};
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+// Cluster0
+// mongoose
+//   .connect(
+//     MONGODB_URL, options
+//   )
+//   .then(result => {
+//      // This should be your user handling code implement following the course videos
+//     app.listen(PORT);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
 app.listen(PORT);
+
+
