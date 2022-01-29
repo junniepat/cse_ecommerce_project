0d35b4700a66c955b0534b6517799c9ef96d296d
junniepat <patrickoigwe@gmail.com>
2022-01-29 21:50:09 +0400

    make it better

--- app.js
+++ app.js
@@ -36,21 +36,21 @@ const options = {
   family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://<username>:<username>@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://junniepat:utchey1995@cluster0.6oqmc.mongodb.net/test?retryWrites=true&w=majority";
 // Cluster0
-// mongoose
-//   .connect(
-//     MONGODB_URL, options
-//   )
-//   .then(result => {
-//      // This should be your user handling code implement following the course videos
-//     app.listen(PORT);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+     // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
-app.listen(PORT);
+// app.listen(PORT);
 
 
