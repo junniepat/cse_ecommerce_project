3c7e2a7c200aaebd9612cd5d2be1636c78a39195
junniepat <patrickoigwe@gmail.com>
2022-01-29 22:13:19 +0400

    mongoatlas user login error

--- app.js
+++ app.js
@@ -41,19 +41,19 @@ const options = {
 const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://patrick:6BcyuIob7wGWGPXh@cluster0.6oqmc.mongodb.net/test?retryWrites=true&w=majority";
 // Cluster0
 
-mongoose
-  .connect(
-    MONGODB_URL, options
-  )
-  .then(result => {
-     // This should be your user handling code implement following the course videos
-    app.listen(PORT);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(
+//     MONGODB_URL, options
+//   )
+//   .then(result => {
+//      // This should be your user handling code implement following the course videos
+//     app.listen(PORT);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
 
 
-// app.listen(PORT);
+app.listen(PORT);
 
 
