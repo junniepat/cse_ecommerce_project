4b9dae2bd7345babb2e66d20849350b06dd5bfa8
junniepat <patrickoigwe@gmail.com>
2022-01-29 22:58:30 +0400

    mongoatlas user login error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const corsOptions = {
+const corsOptions = { 
   origin: "https://cse341-patrick.herokuapp.com/",
   optionsSuccessStatus: 200
 };
@@ -46,6 +46,8 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://patrick:6BcyuIob7w
 //     MONGODB_URL, options
 //   )
 //   .then(result => {
+//     console.log('connected to db')
+
 //      // This should be your user handling code implement following the course videos
 //     app.listen(PORT);
 //   })
@@ -53,7 +55,26 @@ const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://patrick:6BcyuIob7w
 //     console.log(err);
 //   });
 
+  mongoose.connect(MONGODB_URL, 
+    {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  );
 
-app.listen(PORT);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+  app.listen(PORT);
+});
+
+// db.products.insert( { item: "card", qty: 15 } )
+
+
+
+// app.listen(PORT);
 
 
