dd188eee6b2f6fd5ced24b33e47a81cc994d9815
junniepat <patrickoigwe@gmail.com>
2022-02-06 07:19:00 +0400

    changes

--- app.js
+++ app.js
@@ -1,48 +1,77 @@
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const cors = require('cors');
-
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const corsOptions = { 
-  origin: "https://cse341-patrick.herokuapp.com/",
-  optionsSuccessStatus: 200
-};
- 
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
+
+const MONGODB_URI = process.env.MONGODB_URL || "mongodb+srv://patrick:6BcyuIob7wGWGPXh@cluster0.6oqmc.mongodb.net/shop?retryWrites=true&w=majority";
+
 
 const app = express();
-app.use(cors(corsOptions));
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-
-mongoConnect(() => {
-  app.listen(PORT);
-});
-
-
- 
-
-
-
-// app.listen(PORT);
-
-
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
