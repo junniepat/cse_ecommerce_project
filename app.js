88d2f1966242dd181d5a768ac37e14704572b30e
junniepat <patrickoigwe@gmail.com>
2022-02-06 06:11:51 +0400

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 const cors = require('cors');
-const mongoose = require('mongoose');
+
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -13,8 +13,8 @@ const corsOptions = {
 };
  
 
-
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 app.use(cors(corsOptions));
@@ -34,44 +34,12 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 
-const options = {
-  family: 4
-};
-
-const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://patrick:6BcyuIob7wGWGPXh@cluster0.6oqmc.mongodb.net/test?retryWrites=true&w=majority";
-// Cluster0
-
-// mongoose
-//   .connect(
-//     MONGODB_URL, options
-//   )
-//   .then(result => {
-//     console.log('connected to db')
-
-//      // This should be your user handling code implement following the course videos
-//     app.listen(PORT);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-  mongoose.connect(MONGODB_URL, 
-    {
-      useNewUrlParser: true,
-      // useFindAndModify: false,
-      useUnifiedTopology: true
-    }
-  );
-
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+mongoConnect(() => {
   app.listen(PORT);
 });
 
-// db.products.insert( { item: "card", qty: 15 } )
+
+ 
 
 
 
